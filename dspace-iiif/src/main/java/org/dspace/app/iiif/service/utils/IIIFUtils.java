a3a142a6622232187d2291ff27161042a30f748e
Mykhaylo Boychuk <boychuk.michele@gmail.com>
2026-03-12 17:39:17 +0100

    [GLAM-1292] disable canvasdimensions cash

--- dspace-iiif/src/main/java/org/dspace/app/iiif/service/utils/IIIFUtils.java
+++ dspace-iiif/src/main/java/org/dspace/app/iiif/service/utils/IIIFUtils.java
@@ -302,7 +302,7 @@ public class IIIFUtils {
      * @param bitstream the bitstream DSO
      * @return image dimensions
      */
-    @Cacheable(key = "#bitstream.getID().toString()", cacheNames = "canvasdimensions")
+    //@Cacheable(key = "#bitstream.getID().toString()", cacheNames = "canvasdimensions")
     public int[] getImageDimensions(Bitstream bitstream) {
         return iiifApiQueryService.getImageDimensions(bitstream);
     }
