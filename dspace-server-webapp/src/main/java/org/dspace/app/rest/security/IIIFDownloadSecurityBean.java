29d88ac52c5a2fdd504ad6a455bf01b1f6f8297a
Mykhaylo Boychuk <boychuk.michele@gmail.com>
2026-03-19 01:14:35 +0100

    [GLAM-647] improved IIIFDownloadSecurityBean

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/security/IIIFDownloadSecurityBean.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/security/IIIFDownloadSecurityBean.java
@@ -7,15 +7,15 @@
  */
 package org.dspace.app.rest.security;
 
+import static org.dspace.content.Item.ANY;
+
 import java.sql.SQLException;
 import java.util.UUID;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.apache.commons.lang3.StringUtils;
 import org.dspace.app.rest.utils.ContextUtil;
 import org.dspace.authorize.service.AuthorizeService;
 import org.dspace.content.Bitstream;
-import org.dspace.content.Item;
 import org.dspace.content.service.BitstreamService;
 import org.dspace.core.Context;
 import org.slf4j.Logger;
@@ -26,24 +26,36 @@ import org.springframework.stereotype.Component;
 @Component(value = "iiifdownloadSecurity")
 public class IIIFDownloadSecurityBean {
 
-    private static Logger logger = LoggerFactory.getLogger(IIIFDownloadSecurityBean.class);
+    private static Logger log = LoggerFactory.getLogger(IIIFDownloadSecurityBean.class);
 
     @Autowired
     private BitstreamService bitstreamService;
     @Autowired
     private AuthorizeService authorizeService;
 
+    /**
+     * Determine whether the given bitstream can be downloaded.
+     *
+     * @param request       current HTTP request, used to retrieve the DSpace context
+     * @param bitstreamUUID UUID of the bitstream that should be checked
+     * @return true unless the bitstream is marked "nodownload" and the caller is not an admin
+     */
     public boolean isDownloadable(HttpServletRequest request, UUID bitstreamUUID) throws SQLException {
-        Context context = ContextUtil.obtainContext(request);;
+        Context context = ContextUtil.obtainContext(request);
         Bitstream bitstream = bitstreamService.find(context, bitstreamUUID);
         if (bitstream == null) {
+            log.warn("Bitstream with uuid {} not found", bitstreamUUID);
             return true;
         }
-        String value = bitstreamService.getMetadataFirstValue(bitstream,"bitstream", "viewer", "provider", Item.ANY);
-        if (StringUtils.equals("nodownload", value) && !authorizeService.isAdmin(context)) {
-            return false;
-        }
-        return true;
+        return !hasNodownloadValue(bitstream) || authorizeService.isAdmin(context);
+    }
+
+    private boolean hasNodownloadValue(Bitstream bitstream) {
+        return bitstreamService.getMetadata(bitstream,"bitstream", "viewer", "provider", ANY)
+                               .stream()
+                               .filter(mv -> mv.getValue().equals("nodownload"))
+                               .findFirst()
+                               .isPresent();
     }
 
 }
